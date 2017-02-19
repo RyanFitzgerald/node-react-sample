d332a08355c59a8c9a9f1dd524c1ea02802a1b5a
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-02-19 18:42:43 -0500

    Added additional features

---
+++ serverRender.js
@@ -0,0 +1,19 @@
+// Fetch data from api
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App from './src/components/App';
+
+import config from './config';
+import axios from 'axios';
+
+const serverRender = () =>
+    axios.get(`${config.serverURL}/api/contests`)
+        .then(resp => {
+            return ReactDOMServer.renderToString(
+                <App initialContests={resp.data.contests} />
+            );
+        })
+        .catch(console.error);
+
+export default serverRender;
